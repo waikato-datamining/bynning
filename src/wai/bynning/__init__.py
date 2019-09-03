5d170d6cd3910df9c153fc907b1144b12483251f
csterling <coreytsterling@gmail.com>
2019-09-03 16:36:40 +1200

    Moved Extractor to its own package (so utility extractors can be added).

--- src/wai/bynning/__init__.py
+++ src/wai/bynning/__init__.py
@@ -1,6 +1,5 @@
 from ._Bin import Bin
 from ._BinItem import BinItem
-from ._Extractor import Extractor
 from ._Binnable import Binnable
 from ._Binning import Binning
 from ._typing import LabelType, KeyType, ItemType
---
+++ src/wai/bynning/extract/__init__.py
@@ -0,0 +1,1 @@
+from ._Extractor import Extractor
