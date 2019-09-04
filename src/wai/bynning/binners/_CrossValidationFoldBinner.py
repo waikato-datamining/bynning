7c0bb0c4fc2a4bcb9b57c10ee2096e0906d961d9
csterling <coreytsterling@gmail.com>
2019-09-04 13:45:11 +1200

    Reconfigured CrossValidationFoldBinner to move to any fold, and fixed bug in __iter__ method where it was stopping early.

--- src/wai/bynning/binners/_CrossValidationFoldBinner.py
+++ src/wai/bynning/binners/_CrossValidationFoldBinner.py
@@ -1,7 +1,5 @@
 from typing import Optional, Tuple
 
-from wai.common import InvalidStateError
-
 from .._Binnable import Binnable
 from .._typing import LabelType
 from .._typing import KeyType
@@ -23,39 +21,20 @@ class CrossValidationFoldBinner(ConfiguredBinner[KeyType, str]):
 
         super().__init__()
 
-        self._fold: int = 0
         self._num_folds: Optional[int] = num_folds
 
+        # Configured state
         self.__num_items: int = 0
+        self.__fold_size: int = 0
+        self.__num_big_folds: int = 0
+
+        # Per-fold state
+        self.__fold: int = 0
         self.__test_start_index: int = 0
         self.__test_end_index: int = 0
+
+        # Per-item state
         self.__last_index: int = -1
-
-    def next_fold(self) -> int:
-        """
-        Moves this cross-validation fold binner onto the next fold.
-
-        :return:    The index of the next fold (0-based).
-        """
-        # Move to the next fold
-        self._fold += 1
-
-        # If we've moved past the last fold, raise
-        if self._fold >= self._num_folds:
-            raise InvalidStateError(f"Attempted to move to invalid fold {self._fold} (folds = {self._num_folds})")
-
-        # Calculate the start and end index of the next test set
-        test_start_index: int = self.__test_end_index
-        test_end_index: int = test_start_index + (self.__test_end_index - self.__test_start_index)
-        if self._fold == self.__num_items % self._num_folds:
-            test_end_index -= 1
-        self.__test_start_index = test_start_index
-        self.__test_end_index = test_end_index
-
-        # Reset the item index
-        self.__last_index = -1
-
-        return self._fold
 
     def _configure(self, items: Tuple[Binnable[KeyType], ...]):
         # Record the number of items
@@ -68,10 +47,36 @@ class CrossValidationFoldBinner(ConfiguredBinner[KeyType, str]):
         elif self._num_folds > self.__num_items:
             raise ValueError(f"{self._num_folds} requested but only {self.__num_items} provided")
 
-        # Calculate the size of the test-set
-        self.__test_end_index = self.__num_items // self._num_folds
-        if self.__num_items % self._num_folds != 0:
+        # Calculate the size of each fold
+        self.__fold_size = self.__num_items // self._num_folds
+
+        # Calculate the number of folds with one extra item
+        self.__num_big_folds = self.__num_items % self._num_folds
+
+        # Start at fold 0
+        self.set_fold(0)
+
+    def set_fold(self, fold: int):
+        """
+        Sets the fold number to bin against.
+
+        :param fold:    The fold number.
+        """
+        # Can't go outside the range [0, num_folds)
+        if fold < 0 or fold >= self._num_folds:
+            raise ValueError(f"Fold must be in [0,{self._num_folds}), got {fold}")
+
+        # Set the fold number
+        self.__fold = fold
+
+        # Calculate the start and end indices of the test set
+        self.__test_start_index = self.__fold_size * fold + min(self.__num_big_folds, fold)
+        self.__test_end_index = self.__test_start_index + self.__fold_size
+        if fold < self.__num_big_folds:
             self.__test_end_index += 1
+
+        # Reset the binning index
+        self.__last_index = -1
 
     def bin(self, item: Binnable[KeyType]) -> LabelType:
         # Move to the next item index
@@ -84,11 +89,11 @@ class CrossValidationFoldBinner(ConfiguredBinner[KeyType, str]):
             return CrossValidationFoldBinner.TRAIN_BIN_LABEL
 
     def __iter__(self):
-        return self
+        self.set_fold(0)
+        while True:
+            yield self
 
-    def __next__(self):
-        if self._fold < self._num_folds - 1:
-            self.next_fold()
-            return self
-        else:
-            raise StopIteration
+            if self.__fold == self._num_folds - 1:
+                return
+
+            self.set_fold(self.__fold + 1)
